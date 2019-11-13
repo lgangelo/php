360da6febb17ac80b7baeaba64ae0f3feca55bbc
lgangelo <57691185+lgangelo@users.noreply.github.com>
2019-11-13 11:13:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
- $texto = "Olá, \n esta é uma pratica de apresentação do TCC. \n\n Att. \n Angelo Luiz Grande \n Analista de Infraestrutura.";
+ echo "Olá, \n esta é uma pratica de apresentação do TCC. \n\n Att. \n Angelo Luiz Grande \n Analista de Infraestrutura.";
 
- echo $texto;
-?>
+ ?>
