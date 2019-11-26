f95b1b5d3de6b411c59185ba9668e81f270089df
lgangelo <57691185+lgangelo@users.noreply.github.com>
2019-11-26 14:02:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  <p>Esta é uma apresentação do meu TCC</p>
  <p>Angelo Luiz Grande</p>
  <p>Analista de Infraestrutura</p>
- <p>Site Versão 2.4</p>
- <p>Pratica para apresentação!</p>
+ <p>Site Versão 2.5</p>
+ <p>Pratica para apresentação! Demonstração!</p>
  </body>
 </html>
