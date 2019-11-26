0976323cd32ec7e875330a833cf81bacd92ad143
lgangelo <57691185+lgangelo@users.noreply.github.com>
2019-11-26 12:30:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
  <p>Esta é uma apresentação do meu TCC</p>
  <p>Angelo Luiz Grande</p>
  <p>Analista de Infraestrutura</p>
- <p>Site Versão 2.2</p>
+ <p>Site Versão 2.4</p>
+ <p>Pratica para apresentação!</p>
  </body>
 </html>
